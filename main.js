a59646975832bfe22590ba2c5f8833a8ade03e04
jbj01892266 <jbj01892266@taobao.com>
2024-05-23 20:28:50 +0800

    自动打开浏览器

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
+import open from 'open';
 import * as esbuild from 'esbuild';
 
 let ctx = await esbuild.context({
   entryPoints: ['./src/app.ts'],
   bundle: true,
-  loader:{
-    ".wgsl":"text"
+  loader: {
+    '.wgsl': 'text'
   },
   format: 'esm',
   outdir: 'dist'
@@ -13,5 +14,8 @@ let ctx = await esbuild.context({
 await ctx.watch();
 
 let { host, port } = await ctx.serve({
+  port: 8000,
+  host: '127.0.0.1',
   servedir: '.'
 });
+open(`http://${host}:${port}`);
